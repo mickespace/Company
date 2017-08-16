6dab7cd9d3532577a0d3d1d280fccffea0dba19e
mickespace <micke_space@163.com>
2017-08-16 18:07:11 +0800

    company

---
+++ study/angular/heroes/src/app/SimpleFormComponent.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+@Component(
+    {
+        selector: 'app-simple-form',
+        template: '<p> simple-form works!</p>',
+        styles: []
+
+    }
+)
+export class SimpleFormComponent {
+    constructor() {
+
+    }
+}
